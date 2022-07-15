896847cbe4e62eca4d2eb2d030789daa9918d8b5
haytham boussarsar <boussarsarhaytham@gmail.com>
2022-07-15 22:09:20 +0100

    handle fetch data when user login

--- src/redux/global/actions.ts
+++ src/redux/global/actions.ts
@@ -1,6 +1,8 @@
 import api from "@api";
 import { User } from "@api/types/user";
+import { setAlerts } from "@redux/alerts/alertsSlice";
 import { setDevices } from "@redux/devices/devicesSlice";
+import { setReports } from "@redux/reports/reportsSlice";
 import { setSubscriptions } from "@redux/subscriptions/subscriptionsSlice";
 import { setUsers } from "@redux/users/usersSlice";
 import { createAsyncThunk } from "@reduxjs/toolkit";
@@ -35,23 +37,31 @@ export const fetchAll = createAsyncThunk<any, User, { rejectWithValue: any }>("g
       thunkApi.dispatch(setSubscriptions(subscriptions));
       thunkApi.dispatch(setDevices(devices));
       thunkApi.dispatch(setUsers(users));
-      // thunkApi.dispatch(setUsers(users));
-      // thunkApi.dispatch(setUsers(users));
+      thunkApi.dispatch(setReports(reports));
+      thunkApi.dispatch(setAlerts(alerts));
 
     } else {
       const [
         devices,
         users,
+        reports,
+        alerts
       ] = await Promise.all([
         api.devices.find({ params: { p: 'subscription' } }),
         api.users.find({ }),
+        api.reports.find(),
+        api.alerts.find({}),
       ]);
 
       console.log('DEVICES >> ', devices);
       console.log('USERS >> ', users);
+      console.log('REPORTS >> ', reports);
+      console.log('ALERTS >> ', alerts);
 
       thunkApi.dispatch(setDevices(devices));
       thunkApi.dispatch(setUsers(users));
+      thunkApi.dispatch(setReports(reports));
+      thunkApi.dispatch(setAlerts(alerts));
     }
 
     thunkApi.dispatch(setGlobalLoading(false));
