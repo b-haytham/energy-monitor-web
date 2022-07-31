9db73086a640ab194550dad248ad29a349375d15
haytham boussarsar <boussarsarhaytham@gmail.com>
2022-07-31 01:44:56 +0100

    add unit to value card value

--- src/components/cards/ValueCard/ValueCard.tsx
+++ src/components/cards/ValueCard/ValueCard.tsx
@@ -18,20 +18,27 @@ const ValueCard = ({value, sx, ...rest}: ValueCardProps) => {
         minWidth: 200,
         borderRadius: 2,
         mb: 1,
+        height: 1,
         ...sx
       }}
       {...rest}
     >
-      <Stack direction="row" justifyContent={'space-between'} alignItems="center"> 
-        <Typography variant="h6">{value.name}</Typography>
+      <Stack direction="row" justifyContent={'space-between'} alignItems="center" sx={{ mb: 2 }}> 
+        <Typography variant="h5" fontSize={20}>{value.name}</Typography>
         {value.accessor == 's' && (<Circle fontSize="small" color={value.latest_value.value ? "success" : "error"} />)}
       </Stack>
       <Stack>
         {value.accessor == 's' ? 
-        <Typography>-</Typography> : 
-        <Typography >{value.latest_value.value ? value.latest_value.value.toFixed(2): "--.--"  }</Typography>}
+        (
+          <Typography variant="body1" fontSize={20}  sx={{ mb: 1 }}>- </Typography>
+        ) : 
+        (
+          <Typography variant="body1" fontSize={20} sx={{ mb: 1 }}>
+            {value.latest_value.value ? value.latest_value.value.toFixed(2) + ` ${value.unit}` : "--.--"  }
+          </Typography>
+        )}
         
-        <Typography>
+        <Typography variant="body2" >
           {value.latest_value.timestamp ? 
             dayjs(new Date(value.latest_value.timestamp)).format("YYYY/MM/DD hh:mm:ss" ) : 
             '--/--/-- --:--'}
